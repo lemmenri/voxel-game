799039ca4a4e156209fc6e081f6f327f982fc6c9
lemmenri <rick@joberi.nl>
2022-02-23 21:43:48 +0100

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls, Html, useProgress } from "@react-three/drei"
+import House from "./models/House"
+
+function Plane() {
+  return (
+    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  )
+}
+
+function Loader() {
+  const { progress } = useProgress()
+  return <Html center>{progress} % loaded</Html>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="canvas-container">
+      <Canvas>
+        <Suspense fallback={<Loader />}>
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
+          <spotLight
+            intensity={0.5}
+            position={[10, 15, 10]}
+            angle={0.3}
+            castShadow
+          />
+          <Plane receiveShadow />
+          <House />
+        </Suspense>
+      </Canvas>
     </div>
-  );
+  )
 }
 
 export default App;
