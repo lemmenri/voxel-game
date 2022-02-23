cdf31a4e0eb1efbe416c9c6f3e5ad80817405237
lemmenri <rick@joberi.nl>
2022-02-23 22:34:08 +0100

    added three and bench models

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Small game to see what I can do with Three JS Fiber
 ## Preparing OBJ models
 
 To load the objects in Three JS it's recomended to us .GLTF files. These can then be converted to JSX Components. \
-To convert the OBJ model to GLTF use the command `obj2gltf -i model.obj` (see [https://github.com/CesiumGS/obj2gltf](CesiumGS/obj2gltf) for more info)\
+To convert the OBJ model to GLTF use the command `obj2gltf -i model.obj` (see [https://github.com/CesiumGS/obj2gltf](https://github.com/CesiumGS/obj2gltf) for more info)\
 To convert the GLTF model to JSX Component use [https://gltf.pmnd.rs/](https://gltf.pmnd.rs/). The generated code can be used as a react component.\
--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls, Html, useProgress } from "@react-three/drei"
 import House from "./models/House"
+import Tree from "./models/Tree"
+import Bench from "./models/Bench"
 
 function Plane() {
   return (
@@ -20,18 +22,31 @@ function Loader() {
 function App() {
   return (
     <div id="canvas-container">
-      <Canvas>
+      <Canvas frameloop="demand" shadowMap colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
         <Suspense fallback={<Loader />}>
           <OrbitControls />
-          <ambientLight intensity={0.5} />
-          <spotLight
-            intensity={0.5}
-            position={[10, 15, 10]}
-            angle={0.3}
+          <ambientLight intensity={0.5} castShadow />
+          <directionalLight
             castShadow
+            position={[2.5, 8, 5]}
+            intensity={1.5}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            shadow-camera-far={50}
+            shadow-camera-left={-10}
+            shadow-camera-right={10}
+            shadow-camera-top={10}
+            shadow-camera-bottom={-10}
           />
-          <Plane receiveShadow />
-          <House />
+          <Plane receiveShadow rotation-x={-Math.PI / 2} position={[0, -0.5, 0]} args={[10, 10, 4, 4]}>
+            <shadowMaterial attach="material" opacity={0.5} />
+          </Plane>
+          <Plane rotation-x={-Math.PI / 2} position={[0, -0.5, 0]} args={[10, 10, 4, 4]}>
+            <meshBasicMaterial attach="material" opacity={0.5} />
+          </Plane>
+          <House receiveShadow castShadow />
+          <Tree position={[7, 0, 0]} scale={[2, 2, 2]} receiveShadow castShadow />
+          <Bench position={[6, 0, 2]} receiveShadow castShadow />
         </Suspense>
       </Canvas>
     </div>
